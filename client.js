38c128f76736863cd01d6291a8728acff9445d3c
Anaet Hossain <anaethoss@gmail.com>
2017-12-08 00:39:45 +0600

    Client created

---
+++ client.js
@@ -0,0 +1,19 @@
+const net = require('net');
+const HOST = '127.0.0.1';
+const PORT = 6969;
+
+const client = new net.Socket();
+
+client.connect(PORT, HOST, () => {
+  console.log(`CONNECTED TO : ${HOST}:${PORT}`);
+  client.write(`I am chunk norris.`);
+});
+
+client.on('data', data => {
+  console.log(`DATA: ${data}`);
+  client.destroy();
+});
+
+client.on('close', () => {
+  console.log('Connection Closed');
+});
